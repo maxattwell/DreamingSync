3d0a1dd57366f676c9a0c5c9ba90f3fcee083b20
Max Attwell <max.attwell@hotmail.com>
2024-07-10 23:53:21 +1200

    add twitch

--- twitch_videos.js
+++ twitch_videos.js
@@ -1,11 +1,11 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'getData') {
 
-    const videoDuration = document.querySelector('span.ytp-time-duration').innerText
-    const videoCurrent = document.querySelector('span.ytp-time-current').innerText
+    const videoDuration = document.querySelector('p[data-a-target="player-seekbar-duration"]').innerText
+    const videoCurrent = document.querySelector('p[data-a-target="player-seekbar-current-time"]').innerText
 
     const data = {
-      site: 'YouTube',
+      site: 'twitch',
       page: document.title,
       lengthSeconds: convertToSeconds(videoDuration),
       watchedSeconds: convertToSeconds(videoCurrent)
@@ -14,4 +14,3 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
   return true;
 });
-
